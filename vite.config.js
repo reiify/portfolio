0eee2960dd0bc9f89f8792c892f5c35ee1fc40fe
Kagami Rei <dzecksonstorm@gmail.com>
2024-01-17 13:34:26 +0500

    added new dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 100,
+      },
+      cache: false,
+      cacheLocation: undefined,
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
